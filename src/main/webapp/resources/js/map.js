8992d1217534841c2cb268ae46618e4d8abcde7d
kverchi <kverchi24@gmail.com>
2017-08-15 03:57:33 +0200

    Updated map page. Redirecting to sight posts after saving sight.

--- src/main/webapp/resources/js/map.js
+++ src/main/webapp/resources/js/map.js
@@ -1,216 +1,194 @@
-$(document).ready(function(){ 
-/*jQuery('#vmap').vectorMap({
-        map: 'europe_en',
-        enableZoom: false,
-        showTooltip: true,
-        onRegionClick: function(element, code, region){	
-        	//window.location.replace("../sights/country?country_code="+code);
-        	
-        	console.log("country code: " + code);
-        	console.log("region: " + region);
-        	console.log("element: " + element);
-        	window.location.replace("country?country_code="+code);
-        }
-    });*/	
-var isNewSight = false;
+$(document).ready(function(){
 
-var map = new GMaps({
-	  div: '#map',
-	  lat: 49.817492,
-	  lng: 15.472962,
-	  zoom: 7
 });
-var infowindow = new google.maps.InfoWindow();
+function initMap() {
 
-$.get("get-sights", function(data) {
-	for (var i in data) {
-		var sight_id = data[i].sight_id;
-		console.log('sight ID ' + sight_id);
-		map.addMarker({
-			  lat: data[i].map_coord_x,
-			  lng: data[i].map_coord_y,
-			  title: data[i].sight_label,
-			  description: data[i].sight_description,
-			 /* infoWindow: {
-				  content: '<p><a href="./posts/sight/' + data[i].sight_id + '">See more</a> or <button id="update-sight-btn" value="' + data[i].sight_id + '" type="submit"/>Update sight</button></p>'
-			  },*/
-			  id: data[i].sight_id,
-			  click: function(e) {
-				  infowindow.setContent('<p>' + e.title + '</p><p>' + e.description + '</p><a href="../posts/sight/' + e.id + '">See more</a> or <button onclick="updSight(' + e.id + ')" type="submit"/>Update sight</button>');
-				  infowindow.open(map, this);
-				  /*var r = confirm("Do you want to see more about this place?");
-				    if (r == true) {
-				        txt = "You pressed OK!";
-				        window.location.href="posts/" + e.id;
-				        //posts/data[i].sight_id
-				    } else {
-				        txt = "You pressed Cancel!";
-				    }*/
-			  }
-			});
+    var center = {lat: 49.817492, lng: 15.472962};
 
-	}
-	//console.log(data.map_coord_x);
-})
-.done(function() {
-	console.log("done");
-})
-.fail(function(e) {
-	console.log( "error" + e );
-})
-
-$('#geocoding_form').submit(function(e){
-    e.preventDefault();
-    GMaps.geocode({
-      address: $('#address').val().trim(),
-      callback: function(results, status){
-        if(status=='OK'){
-        	var latlng = results[0].geometry.location;
-        	var country_code;
-        	var country_name;
-        	var label = $('#address').val().trim();
-	        raw = results;
-	        //find country name
-	        for (var i=0; i < results[0].address_components.length; i++) {
-	         for (var j=0; j < results[0].address_components[i].types.length; j++) {
-	          if (results[0].address_components[i].types[j] == "country") {
-	            country = results[0].address_components[i];
-	            country_code = country.short_name;
-	            country_name = country.long_name;
-	          }
-	         }
-	        }
-        	
-          
-          map.setCenter(latlng.lat(), latlng.lng());
-          $.get("is-coord-stored", { x: latlng.lat(), y: latlng.lng() }, function(res) {
-        	 if(res == true) {
-        		 isNewSight = false;
-        		 return;
-        	 }
-        		 isNewSight = true;
-                 map.addMarker({
-                   lat: latlng.lat(),
-                   lng: latlng.lng(),
-                   title: $('#address').val(),
-                   click: function(e) {
-                   	 if(isNewSight) {
-       				  var r = confirm("Do you want to save this place?");
-       				    if (r == true) {
-       				       $("#lat").val(latlng.lat());
-       				       $("#lng").val(latlng.lng());
-       				       $("#country_code").val(country_code);
-       				       $("#country_name").val(country_name);
-   				    	   $("#label").val(label);
-       				       $("#modal-form").modal();
-       				       txt = "OK";
-       				       isNewSight = false;
-       				    } else {
-       				        txt = "You pressed Cancel!";
-       				    }
-                   	 }
-                   	 else {
-                   		 infowindow.setContent('<p>' + e.title + '</p>');
-          				 infowindow.open(map, this);
-                   	 }
-       			  }
-                 });
-          });
-        }
-      }
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 6,
+        center: center
     });
-    
-    
-});
 
-//$("#update-sight-btn").click(function() {
-	/*var id = $(this).val();
-	alert(id);
-	var upd_sight_url = "../sights/edit/"+id;
-	$.get(upd_sight_url, function(data) {
-		//event.preventDefault();
-		$("#sight_id").val(id);
-		$("#code").val(data.country_code);
-		$("#label").val(data.sight_label);
-		$("#description").val(data.sight_description);
-    	$("#modal-form").modal();
-	});*/
-//});
+    var infowindow = new google.maps.InfoWindow();
+    var geocoder = new google.maps.Geocoder();
 
-$("#modal-form-save-sight-btn").click(function(event) {
-	event.preventDefault();
+    var sightBaseURL = "/posts/sight/";
 
-	var id = $("#sight_id");
-	var code = $("#country_code");
-	var country_name = $("#country_name");
-	var label = $("#label");
-	var description = $("#description");
-	var lat = $("#lat");
-	var lng = $("#lng");
-	var data = {}
-	var country = {}
-	country["country_code"] = code.val();
-	country["country_name"] = country_name.val();
-	data["sight_id"] = id.val();
-	
-	data["country"] = country;
-	data["sight_label"] = label.val();
-	data["sight_description"] = description.val();
-	data["map_coord_x"] = lat.val();
-	data["map_coord_y"] = lng.val();
-	   
+    loadSights(map, infowindow, sightBaseURL);
+
+
+    $('#geocoding_form').submit(function (e) {
+        e.preventDefault();
+        geocodeAddress(geocoder, map, infowindow);
+    });
+
+    $("#modal-form-save-sight-btn").click(function (event) {
+        event.preventDefault();
+        saveNewSight(map, sightBaseURL);
+    });
+}
+
+function loadSights(map, infowindow, sightBaseURL) {
+    $.get("get-sights", function(data) {
+            for (var i in data) {
+                var sight_id = data[i].sight_id;
+                var marker = new google.maps.Marker({
+                    position: {lat: data[i].map_coord_x, lng: data[i].map_coord_y},
+                    map: map,
+                    title: data[i].sight_label,
+                    description: data[i].sight_description,
+                    id: data[i].sight_id
+                });
+                marker.addListener('click', function() {
+                    infowindow.setContent('<p>' + this.title + '</p><p>' + this.description + '</p><a href=' + sightBaseURL + this.id + '>See more</a> or <button onclick="updSight(' + this.id + ')" type="submit"/>Update sight</button>');
+                    infowindow.open(map, this);
+                });
+            }
+        })
+        .done(function() {
+            console.log("done");
+        })
+        .fail(function(e) {
+            console.log( "error" + e );
+        })
+}
+function geocodeAddress(geocoder, map, infowindow) {
+
+    var address = $('#address').val().trim();
+    geocoder.geocode({'address': address}, function(results, status) {
+        if (status === 'OK') {
+            var latlng = results[0].geometry.location;
+            map.setCenter(latlng);
+            $.get("is-coord-stored", { x: latlng.lat(), y: latlng.lng() }, function(res) {
+                if(res == true) {
+                    return;
+                }
+            });
+            var newMarker = new google.maps.Marker({
+                map: map,
+                position: {lat: latlng.lat(), lng: latlng.lng()},
+                title: address
+            });
+            console.log('new marker title: ' + newMarker.title);
+
+            newMarker.addListener('click', function() {
+               prepareNewSightModalForm(results);
+                $("#modal-form").modal();
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+
+    });
+}
+function prepareNewSightModalForm(results) {
+    var latlng = results[0].geometry.location;
+    var country_code;
+    var country_name;
+
+    //find country name
+    for (var i=0; i < results[0].address_components.length; i++) {
+        for (var j=0; j < results[0].address_components[i].types.length; j++) {
+            if (results[0].address_components[i].types[j] == "country") {
+                country = results[0].address_components[i];
+                country_code = country.short_name;
+                country_name = country.long_name;
+            }
+        }
+    }
+    $("#lat").val(latlng.lat());
+    $("#lng").val(latlng.lng());
+    $("#country_code").val(country_code);
+    $("#country_name").val(country_name);
+    $("#label").val($('#address').val().trim());
+}
+
+function saveNewSight(map, sightBaseURL) {
+    var id = $("#sight_id");
+    var code = $("#country_code");
+    console.log('country code: '+code.val());
+    var country_name = $("#country_name");
+    var label = $("#label");
+    var description = $("#description");
+    var lat = $("#lat");
+    var lng = $("#lng");
+    var data = {}
+    var country = {}
+    country["country_code"] = code.val();
+    country["country_name"] = country_name.val();
+    data["sight_id"] = id.val();
+
+    data["country"] = country;
+    data["sight_label"] = label.val();
+    data["sight_description"] = description.val();
+    data["map_coord_x"] = lat.val();
+    data["map_coord_y"] = lng.val();
+
     tips = $( ".validateTips" );
     var valid = false;
     valid = checkMinMaxLength( code, "code", 2, 2 );
     valid = valid && checkMinMaxLength( label, "label", 3, 80 );
     valid = valid && checkMinMaxLength( description, "description", 3, 80 );
-    
+
     var token = $("meta[name='_csrf']").attr("content");
-	   var header = $("meta[name='_csrf_header']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
     $(document).ajaxSend(function(e, xhr, options) {
-	        xhr.setRequestHeader(header, token);
-	    });
-    
+        xhr.setRequestHeader(header, token);
+    });
+
     var save_sight_url = "../sights/add-sight";
+
     if(valid) {
-    	$.ajax({
-	     	   url: save_sight_url,
-	     	   type:"POST",
-	     	   data: JSON.stringify(data),
-	     	   contentType:"application/json; charset=utf-8",
-	     	   dataType:"json",
-	     	   success: function(obj){
-	     		   hideModalDialog();
-	     		   if(id == 0) {   
-	     			 //new sight was added
-	     			 
-	     		      
-	     		    }
-	     		    
-	     	   },
-	     	   error : function(e) {
-	     		    console.log("Error: ", e);
-	    			display(e);
-	 	   		},
-	 	   		done : function(e) {
-	 	   			alert("DONE");
-	 	   		}
-	     	 });
+        $.ajax({
+            url: save_sight_url,
+            type:"POST",
+            data: JSON.stringify(data),
+            contentType:"application/json; charset=utf-8",
+            dataType:"json",
+            success: function(obj){
+                hideModalDialog();
+                window.location.href = sightBaseURL + obj.sight_id;
+            },
+            error : function(e) {
+                console.log("Error: ", e);
+                display(e);
+            },
+            done : function(e) {
+                alert("DONE");
+            }
+        });
     }
-    
-});
-});
-function updSight(id) { 
-	alert ('ok, ' + id); 
-	var upd_sight_url = "../sights/edit/"+id;
-	$.get(upd_sight_url, function(data) {
-		//event.preventDefault();
-		$("#sight_id").val(id);
-		$("#code").val(data.country_code);
-		$("#label").val(data.sight_label);
-		$("#description").val(data.sight_description);
-		$("#lat").val(data.map_coord_x);
-		$("#lng").val(data.map_coord_y);
-    	$("#modal-form").modal();
-	});	
+}
+function updSight(id) {
+    alert('ok, ' + id);
+    var upd_sight_url = "../sights/edit/" + id;
+    $.get(upd_sight_url, function (data) {
+        //event.preventDefault();
+        $("#sight_id").val(id);
+        console.log('country code: ' + data.country.country_code);
+        $("#country_code").val(data.country.country_code);
+        $("#label").val(data.sight_label);
+        $("#description").val(data.sight_description);
+        $("#lat").val(data.map_coord_x);
+        $("#lng").val(data.map_coord_y);
+        $("#modal-form").modal();
+    });
+}
+function addNewMarker(map, obj) {
+    var infowindow = new google.maps.InfoWindow();
+
+    var newMarker = new google.maps.Marker({
+        map: map,
+        position: {lat: obj.map_coord_x, lng:obj.map_coord_y},
+        title: obj.sight_label,
+        description: obj.sight_description,
+        id: obj.sight_id
+    });
+    console.log('new marker title: ' + newMarker.title);
+
+    newMarker.addListener('click', function() {
+        infowindow.setContent('<p>' + this.title + '</p><p>' + this.description + '</p><a href="../posts/sight/' + this.id + '">See more</a> or <button onclick="updSight(' + this.id + ')" type="submit"/>Update sight</button>');
+        infowindow.open(map, this);
+    });
 }
