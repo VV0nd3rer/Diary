bd0b2eb2b4e8bcf6e1949742dabff67ee680c472
kverchi <kverchi24@gmail.com>
2017-09-12 12:16:31 +0200

    Updated the Welcome Page in Spring Web MVC. For test purporses there are 2  <welcome-file>s: 1. With URL '/', calls Spring MVC controller method. This method re-directs the request and calls corresponding controller for the 'home' URL. 2. With URL 'posts/list', calls directly corresponding controller for the 'home' URL. (Do not work on Heroku)

--- src/main/java/com/kverchi/diary/controller/MainController.java
+++ src/main/java/com/kverchi/diary/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.kverchi.diary.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,6 +22,11 @@ public class MainController {
 	public ModelAndView showDeniedPage() {
 		ModelAndView mv = new ModelAndView("error/denied-error");
 		mv.addObject("result", "Page denied");
+		return mv;
+	}
+	@RequestMapping("/")
+	public ModelAndView showMainPage() {
+		ModelAndView mv = new ModelAndView("index");
 		return mv;
 	}
 }
