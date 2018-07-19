700d7276eb5047df46601b00e56afa39a1974256
kverchi <kverchi24@gmail.com>
2018-07-19 17:25:35 +0200

    Updating pagination on Post page. Updating pagination for getting search results after pressing search button.

--- src/main/resources/static/js/src/posts.js
+++ src/main/resources/static/js/src/posts.js
@@ -1,14 +1,23 @@
 /**
  * Created by Kverchi on 24.6.2018.
  */
-$(document).ready(function(){
 
-    var paginatedElement = $('#post-pagination');
-    var postsUrl = '/posts/paginated-posts';
-    var htmlChangingBlock = 'posts-block';
-    initializePaginationPlagin(paginatedElement, postsUrl, htmlChangingBlock);
+$(document).ready(function () {
 
-    $("#search-post-btn").click(function(e) {
+    var paginationElement = $('#post-pagination');
+    var paginationUrl = '/posts/paginated-posts';
+    var paginationTextChangingBlock = 'posts-block';
+    var searchAttributes;
+
+    initializePaginationPlagin(paginationElement, paginationUrl, paginationTextChangingBlock);
+
+    $("#sightSuggestionInput").focusout(function () {
+        var input = $(this);
+        console.log("focusout: " + $(this).val());
+        checkDataListInput(input);
+    });
+
+    $("#search-post-btn").click(function (e) {
         e.preventDefault();
 
         var sightSuggestionInput = $('#sightSuggestionInput');
@@ -25,45 +34,119 @@ $(document).ready(function(){
             searchAttributes["BY_SIGHT_ID"] = parseInt(sightSuggestionHidden.val());
         }
         /*if (authCorrectInput != null) {
-            searchAttributes["BY_USER_ID"] = parseInt(authSuggestHidden.val());
-        }*/
+         searchAttributes["BY_USER_ID"] = parseInt(authSuggestHidden.val());
+         }*/
         if (searchInTextInput.val() != '') {
             /*searchInTitleOnlyCheckBox.is(":checked") ? searchAttributes["IN_TITLE_ONLY"] = searchText.val() :*/
-                searchAttributes["BY_TEXT"] = searchInTextInput.val();
+            searchAttributes["BY_TEXT"] = searchInTextInput.val();
         }
         /*if (filterSightsDropBox.val() > 0) {
-            filterSightsDropBox.val() == 1 ? postSortType = "BY_WISHES" : postSortType = "BY_VISITS";
-        }*/
-        /*paginationElement.trigger('page');*/
-        console.log('search attrs: ' + JSON.stringify(searchAttributes));
-        initializePaginationPlagin(paginatedElement, "/posts/search-paginated-posts", htmlChangingBlock, searchAttributes);
+         filterSightsDropBox.val() == 1 ? postSortType = "BY_WISHES" : postSortType = "BY_VISITS";
+         }*/
+
+        paginationUrl = "/posts/search-paginated-posts";
+
+        desrtoyPaginationPlagin(paginationElement);
+        initializePaginationPlagin(paginationElement, paginationUrl, paginationTextChangingBlock, searchAttributes);
     });
 });
 
-function initializePaginationPlagin(paginatedElement, postsUrl, htmlChangingBlock, searchAttr) {
+function initializePaginationPlagin(paginationElement, paginationUrl, paginationTextChangingBlock, searchAttributes) {
     var paginationOptions = {
         totalPages: 35,
         visiblePages: 3,
         initiateStartPageClick: false,
         onPageClick: function (event, page) {
-            console.log("pgn clicked " + page);
-            //TODO describe POST method for searching post by searchAttr
-            if(searchAttr === undefined) {
-                console.log('searchAttr is undefined');
-                $.get(postsUrl + "/" + page, function(data){
+            if (typeof searchAttributes === 'undefined') {
+                $.get(paginationUrl + "/" + page, function (data) {
                     console.log("pgn clicked after success " + page);
-                    $("#"+htmlChangingBlock).replaceWith(data);
+                    $("#" + paginationTextChangingBlock).replaceWith(data);
                     currentPage = page;
-                    paginatedElement.twbsPagination('destroy');
-                    paginatedElement.twbsPagination($.extend({}, paginationOptions, {
-                        totalPages: $("#"+htmlChangingBlock).find("span").attr("data-total-pages"),
+                    paginationElement.twbsPagination('destroy');
+                    paginationElement.twbsPagination($.extend({}, paginationOptions, {
+                        totalPages: $("#" + paginationTextChangingBlock).find("span").attr("data-total-pages"),
                         startPage: page
                     }));
                 });
                 console.log("total pages: " + $("#posts-block").find("span").attr("data-total-pages"));
             }
+            else {
+                var postSearchRequest = {};
+                postSearchRequest["searchAttributes"] = searchAttributes;
+                postSearchRequest["currentPage"] = page;
+                $.ajax({
+                    url: paginationUrl,
+                    type: "POST",
+                    data: JSON.stringify(postSearchRequest),
+                    contentType: "application/json; charset=utf-8",
+                    success: function (data) {
+                        console.log("pgn clicked after success " + page);
+                        $("#" + paginationTextChangingBlock).replaceWith(data);
+                        currentPage = page;
+                        paginationElement.twbsPagination('destroy');
+                        paginationElement.twbsPagination($.extend({}, paginationOptions, {
+                            totalPages: $("#" + paginationTextChangingBlock).find("span").attr("data-total-pages"),
+                            startPage: page
+                        }));
+                    },
+                    error: function (e) {
+                        console.log(e);
+                    },
+                    done: function (e) {
+                        //alert("DONE");
+                    }
+                });
+            }
         }
     };
-    paginatedElement.twbsPagination(paginationOptions);
-    paginatedElement.trigger('page', 1);
+    paginationElement.twbsPagination(paginationOptions);
+    paginationElement.trigger('page', 1);
+}
+
+function desrtoyPaginationPlagin(paginationElement) {
+    paginationElement.twbsPagination('destroy');
+}
+
+function checkDataListInput(obj) {
+    var val = obj.val();
+    var hiddenInput = $("#" + obj.attr("id") + '-hidden');
+    var res = false;
+    if (val === '') {
+        hiddenInput.val('');
+        return res;
+    }
+    console.log("checking data list input...");
+
+    var listId = obj.attr('list');
+    var options = $('#' + listId + ' option');
+
+    var hiddenInput = $("#" + obj.attr("id") + '-hidden');
+
+
+    tips = $(".validateTips");
+    res = isDataListInput(options, val, hiddenInput);
+    if (res) {
+        tips.hide();
+    }
+    else {
+        tips.show();
+    }
+    return res;
+}
+function isDataListInput(options, val, hiddenInput) {
+    var res = false;
+
+    for (var i = 0; i < options.length; i++) {
+        var option = options[i];
+        if (option.innerText === val) {
+            console.log("valid");
+            var dataValAttr = option.getAttribute('data-value');
+            if (dataValAttr != null) {
+                hiddenInput.val(dataValAttr);
+            }
+            res = true;
+            break;
+        }
+    }
+    return res;
 }
