25f0abd7cf86ff8d06118548070251a9f59af9fa
Liudmyla Melnychuk <kverchi24@gmail.com>
2019-02-26 20:15:26 +0100

    Updated layout header.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LayoutComponent } from './ui/layout/layout.component';
+import { HeaderComponent } from './ui/header/header.component';
+import { FooterComponent } from './ui/footer/footer.component';
 import { LogoutComponent } from './logout/logout.component';
 import { RegistrationComponent } from './registration/registration.component';
-import { UiModule } from './ui/ui.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     LogoutComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule,
-    UiModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
